c204475c47f3e216569eaf41d3b503b282e81bc9
Sowndarya <sowndhuabi93@gmail.com>
2025-04-17 20:17:36 +0530

    method created

---
+++ src/com/test/MethodsCreation.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class MethodsCreation {
+private void m1() {
+	System.out.println(1);
+
+}
+public static void main(String[] args) {
+	MethodsCreation m = new MethodsCreation();
+	m.m1();
+	
+}
+}
