e56ad65914c40d3adbe41d48c95ef1fd5e039f01
Sowndarya <sowndhuabi93@gmail.com>
2025-04-17 20:21:34 +0530

    created another method

--- src/com/test/MethodsCreation.java
+++ src/com/test/MethodsCreation.java
@@ -5,9 +5,13 @@ private void m1() {
 	System.out.println(1);
 
 }
+private void m2() {
+	System.out.println(2);
+
+}
 public static void main(String[] args) {
 	MethodsCreation m = new MethodsCreation();
 	m.m1();
-	
+	m.m2();
 }
 }
